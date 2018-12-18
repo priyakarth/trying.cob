84e08bbf1a1437211e9d2a0acd3f738c673e075f
priyakarth <45675945+priyakarth@users.noreply.github.com>
2018-12-18 14:59:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "Run \"cobc -x hello-world.cob" to compile.\n\n----------------------------------\n"
+printf "Run \"cobc -x hello-world.cob".\n\n----------------------------------\n"
