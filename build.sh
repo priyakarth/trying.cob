2772d81897f5a0cc8ccf3244c74bcc66caae358c
priyakarth <45675945+priyakarth@users.noreply.github.com>
2018-12-19 09:45:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/centos/.local/bin:/home/centos/bin
 echo `pwd`
-cobc -x hello-world.cob
+for i in *.cob
+do
+cobc -x $i
+done
+
 
 
